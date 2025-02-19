79391deb530c501ba336de0754b641dbb9d6e4d2
Lubaba-Bedewi <lubababadewi@gmail.com>
2025-02-19 07:09:01 -0500

    Rows and Banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import "./App.css";
 import Home from "./Pages/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="App">
-        <>Home</>
+        <Home />
       </div>
     </>
   );
